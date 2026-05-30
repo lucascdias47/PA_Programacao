3ccb7d1174d27a5d867f787349742867b56ef4b8
Lucas Dias <lucas.cdias47@gmail.com>
2026-05-29 21:43:22 -0300

    Usando scanner

--- vetores/Vetores/src/App.java
+++ vetores/Vetores/src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner leia = new Scanner (System.in);
+
+        System.out.println("Digite seu nome :");
+        String nome = leia.nextLine();
+
+        System.out.printf("o nome digitado foi : %s",nome );
+
+        leia.close();
+
+        
     }
 }
